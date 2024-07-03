7bf8dab63949337f3d49839e0b42c8d9934c1616
Davidmrn5 <davidmartinpest@yahoo.es>
2024-07-03 09:41:18 +0200

    added documentation format

--- src/cut_times.py
+++ src/cut_times.py
@@ -5,25 +5,22 @@ from scipy.stats import expon
 
 def cutpoints_AB(n_int_AB, t_AB, coal_AB):
     """
-    This function returns a the cutpoints for the
+    This function returns the cutpoints for the
     intervals for the two-sequence CTMC. The cutpoints
     will be defined by the quantiles of a truncated
     exponential distribution.
 
-    Parameters
-    ----------
-    n_int_AB : integer
-        Number of intervals in the two-sequence CTMC.
-    t_AB : float
-        Total time interval of the two-sequence CTMC
-    coal_AB : float
-        coalescent rate of the two-sequence CTMC.
+    :param n_int_AB: Number of intervals in the two-sequence CTMC.
+    :type n_int_AB: int
+    :param t_AB: Total time interval of the two-sequence CTMC
+    :type t_AB: float
+    :param coal_AB: coalescent rate of the two-sequence CTMC.
+    :type coal_AB: float
+    :return: cut_AB
+    :rtype: np.array
     """
-    # Define probabilities for quantiles
     quantiles_AB = np.array(list(range(n_int_AB + 1))) / n_int_AB
-    # Define truncexpon shape parameters
     lower, upper, scale = 0, t_AB, 1 / coal_AB
-    # Get quantiles
     cut_AB = truncexpon.ppf(
         quantiles_AB, b=(upper - lower) / scale, loc=lower, scale=scale
     )
@@ -32,21 +29,19 @@ def cutpoints_AB(n_int_AB, t_AB, coal_AB):
 
 def cutpoints_ABC(n_int_ABC, coal_ABC):
     """
-    This function returns a the cutpoints for the
+    This function returns the cutpoints for the
     intervals for the three-sequence CTMC. The cutpoints
     will be defined by the quantiles of an exponential
     distribution.
 
-    Parameters
-    ----------
-    n_int_ABC : integer
-        Number of intervals in the three-sequence CTMC.
-    coal_ABC : float
-        coalescent rate of the three-sequence CTMC.
+    :param n_int_ABC: Number of intervals in the three-sequence CTMC.
+    :type n_int_ABC: int
+    :param coal_ABC: coalescent rate of the three-sequence CTMC.
+    :type coal_ABC: float
+    :return: cut_ABC
+    :rtype: np.array
     """
-    # Define probabilities for quantiles
     quantiles_AB = np.array(list(range(n_int_ABC + 1))) / n_int_ABC
-    # Get quantiles
     cut_ABC = expon.ppf(quantiles_AB, scale=1 / coal_ABC)
     return cut_ABC
 
